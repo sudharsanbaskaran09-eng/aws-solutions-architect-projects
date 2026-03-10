9293c00ce496eace7a7fe8d31633d24a6484baa4
Sudharsan B <sudharsanbaskaran09@gmail.com>
2026-03-10 10:52:03 +0530

    Update lambda_function.py

--- project-03-serverless-image-processing/lambda_function.py
+++ project-03-serverless-image-processing/lambda_function.py
@@ -1,1 +1,11 @@
+import json
 
+def lambda_handler(event, context):
+    
+    print("Image uploaded to S3!")
+    print("Event:", json.dumps(event))
+    
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Image event processed successfully!')
+    }
