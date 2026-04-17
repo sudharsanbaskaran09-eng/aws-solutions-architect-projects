81dceb6955ef77c002a222f9608a886a43301744
Sudharsan B <sudharsanbaskaran09@gmail.com>
2026-04-17 21:03:15 +0530

    Update main.tf

--- project-08-Infrastructure-as-Code-Deployment/main.tf
+++ project-08-Infrastructure-as-Code-Deployment/main.tf
@@ -5,24 +5,3 @@ provider "aws" {
 resource "aws_iam_role" "ssm_role" {
   name = "ec2-ssm-role-new"
 
-  role       = aws_iam_role.ssm_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
-}
-
-# Instance Profile
-resource "aws_iam_instance_profile" "ssm_profile" {
-  name = "ec2-ssm-profile-new"
-  role = aws_iam_role.ssm_role.name
-}
-
-              #!/bin/bash
-              yum update -y
-              amazon-linux-extras install nginx1 -y
-              systemctl start nginx
-              systemctl enable nginx
-              EOF
-
-  tags = {
-    Name = "terraform-nginx-instance"
-  }
-}
