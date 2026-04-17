0c3ce8e66b5bae88319953240df725a7e5315433
Sudharsan B <sudharsanbaskaran09@gmail.com>
2026-04-17 21:02:47 +0530

    Remove security group rules from main.tf
    Removed ingress and egress rules from AWS provider configuration.

--- project-08-Infrastructure-as-Code-Deployment/main.tf
+++ project-08-Infrastructure-as-Code-Deployment/main.tf
@@ -1,29 +1,5 @@
 provider "aws" {
-  region = "us-east-1"
-
-  ingress {
-    description = "HTTP"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    description = "SSH"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
+  region = "us-east
 
 # IAM Role for SSM
 resource "aws_iam_role" "ssm_role" {
